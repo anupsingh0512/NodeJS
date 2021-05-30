af2e9e8f1fec9a07559952172003db93b864258b
Anup Singh <anupbharadwaj54@gmail.com>
2021-05-30 23:55:00 +0530

    Node HTTp Example 2

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-<<<<<<< HEAD
 const http = require("http");
-
+const fs = require("fs");
+const path = require("path");
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
+  console.log("Request for " + req.url + " by method " + req.method);
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text-html");
-  res.end("<html><body><h1>Hello,World!</h1></body></html>");
+  if (req.method == "GET") {
+    var fileUrl;
+    if (req.url == "/") fileUrl = "/index.html";
+    else fileUrl = req.url;
+
+    var filePath = path.resolve("./public" + fileUrl);
+    const fileExt = path.extname(filePath);
+    if (fileExt == ".html") {
+      fs.exists(filePath, exists => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.end(
+            "<html><body><h1>Error 404: " +
+              fileUrl +
+              " not found</h1></body></html>"
+          );
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end(
+        "<html><body><h1>Error 404: " +
+          fileUrl +
+          " not a HTML file</h1></body></html>"
+      );
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      "<html><body><h1>Error 404: " +
+        req.method +
+        " not supported</h1></body></html>"
+    );
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
-=======
-var rect = require("./rectangle");
-
-function solveRect(l, b) {
-  console.log("Solving for rectangle with l = " + l + " and b = " + b);
-  rect(l, b, (err, rectangle) => {
-    if (err) {
-      console.log("ERROR: ", err.message);
-    } else {
-      console.log(
-        "The area of the rectangle of dimensions l = " +
-          l +
-          " and b = " +
-          b +
-          " is " +
-          rectangle.area()
-      );
-      console.log(
-        "The perimeter of the rectangle of dimensions l = " +
-          l +
-          " and b = " +
-          b +
-          " is " +
-          rectangle.perimeter()
-      );
-    }
-  });
-  console.log("This statement after the call to rect()");
-}
-
-solveRect(2, 5);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(-3, 5);
->>>>>>> d5d3b70afdbfcd204feb9a9abac05af554feb15e
